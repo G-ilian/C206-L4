bbc758d46283e10d4fd621a8211997f07aa85fa1
G-Ilian <gbiel.boza@gmail.com>
2023-02-14 20:43:59 -0300

    Segunda versão

--- Aulas/Aula 01 - Introdução ao Java/src/main/java/Main.java
+++ Aulas/Aula 01 - Introdução ao Java/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World !!!");
+        System.ou.print("Hello World !!!");
         System.out.println("Segunda parte");
 
     }
